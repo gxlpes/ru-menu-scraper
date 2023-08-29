45161af8d0180fbab2a0699de462e616d69e0f3c
Guilherme Lopes <guilhermxlopes@gmail.com>
2023-08-28 22:15:17 -0300

    first example parsing data

--- ru-scraper-api/src/main/java/com/scraper/ruscraperapi/service/ScrapService.java
+++ ru-scraper-api/src/main/java/com/scraper/ruscraperapi/service/ScrapService.java
@@ -1,5 +1,6 @@
 package com.scraper.ruscraperapi.service;
 
+import com.scraper.ruscraperapi.dto.MenuMeals;
 import com.scraper.ruscraperapi.scrap.ConnectionManager;
 import com.scraper.ruscraperapi.scrap.Utils;
 import org.jsoup.nodes.Document;
@@ -8,7 +9,11 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+
+import static com.scraper.ruscraperapi.service.MenuParser.parseMenuByMeal;
 
 @Service
 public class ScrapService {
@@ -33,6 +38,20 @@ public class ScrapService {
 
         System.out.println(menuFromWeekday);
 
+        Map<String, List<MenuItem>> menuByMeal = parseMenuByMeal(menuFromWeekday);
+
+        for (Map.Entry<String, List<MenuItem>> entry : menuByMeal.entrySet()) {
+            System.out.println("Meal: " + entry.getKey());
+            List<MenuItem> menuItems = entry.getValue();
+            for (MenuItem menuItem : menuItems) {
+                System.out.println("  Title: " + menuItem.getTitle());
+                System.out.println("  Icons: " + menuItem.getIcons());
+            }
+            System.out.println();
+        }
+
         return Optional.of("opa");
     }
+
+
 }
